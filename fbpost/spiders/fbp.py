87397f71fec45aa0117a871e264fe3f18011b767
TrangPhuongMai <33345430+TrangPhuongMai@users.noreply.github.com>
2021-05-23 18:15:16 +0700

    Update fbp.py

--- fbpost/spiders/fbp.py
+++ fbpost/spiders/fbp.py
@@ -6,33 +6,35 @@ import pandas as pd
 from scrapy.loader import ItemLoader
 from fbpost.items import FbpostItem
 
+
 class FbpSpider(scrapy.Spider):
     name = 'fbp'
-    urls = pd.read_csv('/home/nero/PycharmProjects/fbpost/mbasic.csv')
+    urls = pd.read_csv('mbasic.csv')
     start_urls = urls['URL'][:10].array
+
     def __init__(self, *args, **kwargs):
         self.cookies = {
-            'xs': '41%3Atzu2RO5kFIM5WQ%3A2%3A1577019585%3A8613%3A5755',
-            'c_user': '100041368987839',
-            'datr': 'aSHpXZB1y6WSztrUuQox29_Z',
-            'fr': '13DlBHb6iVc5WPK9J.AWUS_ATrbYXgHnK9JI836eoxcxI.BddYQf.ev.F4H.0.0.BeEjY9.AWV7z1Xg'
+            'sb': 'jcSZXjnWuDiodF5GingAJqUP',
+            'datr': 'jcSZXhzwmbqJ7Cuokfib6kPz',
+            'c_user': '100007650168648',
+            'xs': '7%3AcTdXQ4unT4wCSA%3A2%3A1587135637%3A-1%3A6383%3A%3AAcXHi-Dmrw0LLwGL1HJ7UMZtlOuBG7DzZo7_Xl0WP0DX',
+            'fr': '19rGD8BBYRag44HvL.AWUlCibyw8MbZU1tCbeR9jKU9Zk.Bgpq9C.Yz.GCo.0.0.Bgpq9C.',
         }
 
     def parse(self, response):
         for url in self.start_urls:
-            l = ItemLoader(item=FbpostItem(),response=response)
-            l.add_value('post_id',url)
-            yield Request(url, cookies=self.cookies, meta={'item': l,'proxy': "http://103.143.206.17:443"},callback=self.parse_p)
-    
-    def parse_p(self,response):
+            l = ItemLoader(item=FbpostItem(), response=response)
+            l.add_value('post_id', url)
+            yield Request(url, cookies=self.cookies, meta={'item': l}, callback=self.parse_p)
 
-        # logging.info(response.xpath('//div//text()').extract())
+    def parse_p(self, response):
         check_reactions = response.xpath("//a[contains(@href,'reaction/profile')]//text()").get()
-        # logging.info("check reactions {}".format(check_reactions))
+        logging.info("check reactions {}".format(check_reactions))
         date = response.xpath("//div[@id='m_story_permalink_view']//abbr/text()").extract()
         l = ItemLoader(item=FbpostItem(), response=response, parent=response.meta['item'])
+
         try:
-            l.add_xpath('date',"//div[@id='m_story_permalink_view']//abbr/text()")
+            l.add_xpath('date', "//div[@id='m_story_permalink_view']//abbr/text()")
         except:
             pass
 
@@ -40,11 +42,11 @@ class FbpSpider(scrapy.Spider):
             logging.info("yield new.load_item()")
         else:
             # new.add_xpath('reactions', "//a[contains(@href,'reaction/profile')]/div/div/text()")
-            reactions = response.xpath("//div[contains(@id,'sentence')]/a[contains(@href,'reaction/profile')]/@href").extract()
+            reactions = response.xpath(
+                "//div[contains(@id,'sentence')]/a[contains(@href,'reaction/profile')]/@href").extract()
             reactions = response.urljoin(reactions[0])
             logging.info(reactions)
-            yield scrapy.Request(reactions,cookies=self.cookies, callback=self.parse_reactions,meta={'item':l})
-
+            yield scrapy.Request(reactions, cookies=self.cookies, callback=self.parse_reactions, meta={'item': l})
 
     def parse_reactions(self, response):
         # logging.info("============= parse_reactions ==============")
@@ -62,6 +64,5 @@ class FbpSpider(scrapy.Spider):
             l.add_value('like', s.split('total_count=')[-1].split('&')[0])
         else:
             l.add_value('like', 0)
-
 
         return l.load_item()
